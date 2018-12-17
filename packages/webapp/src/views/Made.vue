ccbca67fe6f2f89fdbafe7f0a7d7d58530922e18
AHN HO JUNG <labeldock@me.com>
2018-12-17 22:24:41 +0900

    change to history mode

--- packages/webapp/src/views/Made.vue
+++ packages/webapp/src/views/Made.vue
@@ -8,3 +8,20 @@
     </div>
   </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { State } from 'vuex-class';
+
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+
+@Component({
+  components: {}
+})
+export default class Home extends Vue {
+  @State('armors') armors;
+  @State('charms') charms;
+  @State('items') items;
+  @State('skills') skills;
+}
+</script>
