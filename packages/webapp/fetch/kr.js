ab2162905022ae608d10bac043a8ca4bd98ef11b
AHN HO JUNG <labeldock@me.com>
2018-12-17 22:21:18 +0900

    fix indent

--- packages/webapp/fetch/kr.js
+++ packages/webapp/fetch/kr.js
@@ -6,23 +6,23 @@ const baseUrl = "http://www.mhwdb.kr/apis";
 const dbPath = path.join(__dirname, "../db");
 
 const categories = {
-    skills:`${baseUrl}/skills`,
-    items:`${baseUrl}/items`,
-    armors:`${baseUrl}/armors`,
-    charms:`${baseUrl}/charms`
+  skills:`${baseUrl}/skills`,
+  items:`${baseUrl}/items`,
+  armors:`${baseUrl}/armors`,
+  charms:`${baseUrl}/charms`
 };
 
 
 Object.keys(categories).map(async categoryName=>{
-    const categoryUrl = categories[categoryName];
-    console.log(`fetch ${categoryName} :: ${categoryUrl}`);
+  const categoryUrl = categories[categoryName];
+  console.log(`fetch ${categoryName} :: ${categoryUrl}`);
 
-    try {
-        const { data } = await axios.get(categoryUrl);
-        const content = JSON.stringify(data,2,2);
-        fs.writeFileSync(path.join(dbPath,`/${categoryName}.json`),content);
-    } catch(e){
-        console.log("Request Error", e);
-    }
+  try {
+    const { data } = await axios.get(categoryUrl);
+    const content = JSON.stringify(data,2,2);
+    fs.writeFileSync(path.join(dbPath,`/${categoryName}.json`),content);
+  } catch(e){
+    console.log("Request Error", e);
+  }
     
 });
