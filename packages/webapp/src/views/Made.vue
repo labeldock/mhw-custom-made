c97482f91e15a20aadedeef6f6d23002af161e03
AHN HO JUNG <labeldock@me.com>
2018-12-18 22:08:21 +0900

    update

--- packages/webapp/src/views/Made.vue
+++ packages/webapp/src/views/Made.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="about">
-    <div>
+  <div class="page-custommade">
+    
+    <div class="wanted">
       Wanted
       <div>
         <input type="text" v-model="wantedText">
         <br>
-        <select multiple @input="selectedWantedItem">
-          <option v-for="{idx, name} in filteredSkills" :key="idx" :value="idx">{{ name }}</option>
-        </select>
+        <ul class="wanted-list" @input="selectedWantedItem">
+          <li class="wanted-list-item" v-for="{idx, name, desc} in filteredSkills" :key="'wl'+idx" :value="idx">
+            <div class="name">{{ name }}</div>
+            <div class="desc">
+              <div v-for="{ name, desc } in desc" :key="idx+name">
+                {{ name }} {{ desc }}
+              </div>
+            </div>
+            <div class="controls">
+              <button @click="wantedSkillWithIdx(idx)">추가</button>
+            </div>
+          </li>
+        </ul>
         <div>
           {{ item }}
         </div>
       </div>
+    </div>
+    <div class="suggest">
+      <ul class="wanted-skills">
+        <li class="wanted-skills-item" v-for="{idx, name, desc} in wantedSkills" :key="'ws'+idx">
+          <div class="name">{{ name }}</div>
+          <div class="desc">
+            <div v-for="{ name, desc } in desc" :key="idx+name">
+              <input type="radio" :name="`ws${idx}${name}`">{{ name }} {{ desc }}
+            </div>
+          </div>
+          <div class="controls">
+            <button @click="wantedSkillWithIdx(idx)">제외</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="equip">
       
     </div>
   </div>
@@ -35,22 +63,41 @@ export default class Home extends Vue {
   
   wantedText:string="";
   item:any=null;
+  wantedSkills:Array<any>=[];
   
   get filteredSkills ():Array<{ idx:number, name:string }> {
     
-    
+    const wantedSkills = this.wantedSkills;
     const wantedText = this.wantedText;
-    const skills = this.skills;
+    const skills = this.skills.filter(({ idx })=>{
+      return !wantedSkills.some(({ idx:alreadyIdx })=>alreadyIdx===idx);
+    });
+
+    if(wantedSkills){
+      
+    }
     
     if(!wantedText){
       return skills;
     }
     
     const searcher = new Hangul.Searcher(wantedText);
-    
-    return skills.filter(({ name })=>{
-      return searcher.search(name) === 0;
+    const searched = skills.filter(({ name, desc },index)=>{
+      const content = `${name} ${desc.map(({desc})=>desc).join("")}`;
+      const matched = searcher.search(content) === 0 || content.indexOf(wantedText) != -1;
+      return matched;
     });
+    
+    return searched;
+  }
+  
+  wantedSkillWithIdx (wantedIdx){
+    const skills = this.skills;
+    const wantedSkills = this.wantedSkills;
+    
+    const skill = skills.find(({idx})=>idx===wantedIdx);
+    
+    wantedSkills.push({ ...skill });
   }
   
   selectedWantedItem (datum){
@@ -62,3 +109,71 @@ export default class Home extends Vue {
   
 }
 </script>
+
+<style lang="scss">
+  .page-custommade {
+    display: flex;
+    flex-flow: row nowrap;
+    text-align:left;
+    
+    .wanted {
+      flex: 1;
+      background-color:#cee;
+      .wanted-list {
+        overflow:auto;
+        height:70vh;
+        min-height:600px;
+        .wanted-list-item {
+          position:relative;
+          .name {
+            font-weight:bold;
+          }
+          .desc {
+            font-size:12px;
+          }
+          .controls {
+            position:absolute;
+            right:5px;
+            top:50%;
+            transform:translateY(-50%);
+          }
+          + .wanted-list-item {
+            margin-top:10px;
+          }
+        }
+      }
+    }
+    .suggest {
+      flex: 1;
+      background-color:#ece;
+      .wanted-skills {
+        overflow:auto;
+        height:70vh;
+        min-height:600px;
+        .wanted-skills-item {
+          position:relative;
+          .name {
+            font-weight:bold;
+          }
+          .desc {
+            font-size:12px;
+          }
+          .controls {
+            position:absolute;
+            right:5px;
+            top:50%;
+            transform:translateY(-50%);
+          }
+          + .wanted-skills-item {
+            margin-top:10px;
+          }
+        }
+      }
+    }
+    .equip {
+      flex: 1;
+      background-color:#eec;
+    }
+    
+  }
+</style>
